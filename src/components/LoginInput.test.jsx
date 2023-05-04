39cfcc13a5e7f6a4dc02e0e0c45508feda2069dd
arsat teguh <arsyatteguh@gmail.com>
2023-05-04 10:08:12 +0700

    changes

--- src/components/LoginInput.test.jsx
+++ src/components/LoginInput.test.jsx
@@ -7,55 +7,55 @@
  *   - should call login function when login button is clicked
  */
 
-import React from 'react';
-import { act, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import LoginInput from './LoginInput';
+import React from "react";
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import LoginInput from "./LoginInput";
 
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
-describe('LoginInput component', () => {
-  it('should handle username typing correctly', async () => {
+describe("LoginInput component", () => {
+  it("should handle username typing correctly", async () => {
     // Arrange
     await act(async () => render(<LoginInput login={() => {}} />));
-    const usernameInput = await screen.getByPlaceholderText('Username');
+    const usernameInput = await screen.getByPlaceholderText("Username");
 
     // Action
-    await act(async () => userEvent.type(usernameInput, 'usernametest'));
+    await act(async () => userEvent.type(usernameInput, "usernametest"));
 
     // Assert
-    expect(usernameInput).toHaveValue('usernametest');
+    expect(usernameInput).toHaveValue("usernametest");
   });
 
-  it('should handle password typing correctly', async () => {
+  it("should handle password typing correctly", async () => {
     // Arrange
     await act(async () => render(<LoginInput login={() => {}} />));
-    const passwordInput = await screen.getByPlaceholderText('Password');
+    const passwordInput = await screen.getByPlaceholderText("Passwords");
 
     // Action
-    await act(async () => userEvent.type(passwordInput, 'passwordtest'));
+    await act(async () => userEvent.type(passwordInput, "passwordtest"));
 
     // Assert
-    expect(passwordInput).toHaveValue('passwordtest');
+    expect(passwordInput).toHaveValue("passwordtest");
   });
 
-  it('should call login function when login button is clicked', async () => {
+  it("should call login function when login button is clicked", async () => {
     // Arrange
     const mockLogin = jest.fn();
     await act(async () => render(<LoginInput login={mockLogin} />));
-    const usernameInput = await screen.getByPlaceholderText('Username');
-    await act(async () => userEvent.type(usernameInput, 'usernametest'));
-    const passwordInput = await screen.getByPlaceholderText('Password');
-    await act(async () => userEvent.type(passwordInput, 'passwordtest'));
-    const loginButton = await screen.getByRole('button', { name: 'Login' });
+    const usernameInput = await screen.getByPlaceholderText("Username");
+    await act(async () => userEvent.type(usernameInput, "usernametest"));
+    const passwordInput = await screen.getByPlaceholderText("Password");
+    await act(async () => userEvent.type(passwordInput, "passwordtest"));
+    const loginButton = await screen.getByRole("button", { name: "Login" });
 
     // Action
     await act(async () => userEvent.click(loginButton));
 
     // Assert
     expect(mockLogin).toBeCalledWith({
-      id: 'usernametest',
-      password: 'passwordtest',
+      id: "usernametest",
+      password: "passwordtest",
     });
   });
 });
